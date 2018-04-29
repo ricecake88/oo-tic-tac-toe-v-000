8fbcf2a074cdcb4c836ee70c6399ca717e43b5af
Grace Shih <vocalise@gmail.com>
2018-04-29 16:28:27 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -51,7 +51,7 @@ class TicTacToe
   def turn_count
     count = 0
     @board.each do|turn|
-      if @board[turn] == "X" || @board[turn] == "O"
+      if turn == "X" || turn == "O"
         count += 1
       end
     end
