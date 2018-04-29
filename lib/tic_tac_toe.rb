601bfb4ecb129f785eec4d3f23f37587c8ec8e56
Grace Shih <vocalise@gmail.com>
2018-04-29 16:35:29 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -67,8 +67,7 @@ class TicTacToe
   end
 
   def turn(position)
-    position = gets.strip
-    index = input_to_index(position)
+    index = input_to_index(gets.strip)
     if index < 9 && index >= 0
       move(index, current_player)
     else
